a2545f0c5afb8b81f7b30ca9b045eee21c327ba4
MrM <MMMohebi@outlook.com>
2023-01-07 01:57:58 -0800

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module printer-cmd
+
+go 1.19
+
+require github.com/jadefox10200/goprint v0.0.0-20220906024909-7bed801d01d2
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"github.com/jadefox10200/goprint"
+)
+
+func main() {
+
+	filePath := flag.String("f", "", "[REQUIERD] File path to PDF")
+
+	defaultPrinterName, _ := goprint.GetDefaultPrinterName()
+	printerName := flag.String("p", defaultPrinterName, "Specifies custom printer name. if NOT set default prrinter will be used")
+
+	flag.Parse()
+
+	if len([]rune(*filePath)) < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	fmt.Println(*printerName)
+
+	//open the printer
+	printerHandle, err := goprint.GoOpenPrinter(*printerName)
+	if err != nil {
+		log.Fatalln("Failed to open printer")
+	}
+	defer goprint.GoClosePrinter(printerHandle)
+
+	//Send to printer:
+	err = goprint.GoPrint(printerHandle, *filePath)
+	if err != nil {
+		log.Fatalln("during the func sendToPrinter, there was an error")
+	}
+
+}
